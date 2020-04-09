53fc80679687679696468b02120b142d8109b49d
Parag Chirde <parag.chirde@gmail.com>
2020-04-09 17:35:56 +0530

    Added Credits and Info in Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="text-center">
       <router-link to="/">Home</router-link> |
       <router-link to="/lottery">Lottery Game</router-link>
     </div>
