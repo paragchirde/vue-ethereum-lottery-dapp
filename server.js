e91f5672a2797e63f63e27d5cb40cf49419faf45
Parag Chirde <parag.chirde@gmail.com>
2020-04-11 11:56:34 +0530

    Heroku Vue Router Bug fixed

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const history = require('connect-history-api-fallback')
 
 const app = express()
+
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
